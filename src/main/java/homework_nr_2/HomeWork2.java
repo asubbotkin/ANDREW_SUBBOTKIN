cb90e6791f1e6a414ab5a31c8b7589ffcab862f9
Andrew Subbotkin <asubbotkin@gmail.com>
2024-03-26 20:16:11 +0200

    Declaration and output of variables of primitive types

--- src/main/java/homework_nr_2/HomeWork2.java
+++ src/main/java/homework_nr_2/HomeWork2.java
@@ -1,7 +1,22 @@
 package homework_nr_2;
 
+// 3. Created class HomeWork2
 public class HomeWork2 {
     public static void main(String[] args) {
+        // 4. Output to the terminal string "Hello World!"
         System.out.println("Hello World!");
+
+        // 5. Declaration and output of variables of primitive types
+        long currentWorldPopulation = 7811080815L;
+        int carProducedThisYear = 55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaAmazing = true;
+        float pi = 3.14f;
+
+        System.out.println(currentWorldPopulation);
+        System.out.println(carProducedThisYear);
+        System.out.println(firstLetterInAlphabet);
+        System.out.println(javaAmazing);
+        System.out.println(pi);
     }
 }
