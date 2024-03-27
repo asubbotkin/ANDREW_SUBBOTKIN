8c9293ecc565a3dbadedebbff2654882df883936
Andrew Subbotkin <asubbotkin@gmail.com>
2024-03-27 10:34:49 +0200

    homework_nr_2

--- src/main/java/homework_nr_2/HomeWork2.java
+++ src/main/java/homework_nr_2/HomeWork2.java
@@ -13,10 +13,35 @@ public class HomeWork2 {
         boolean javaAmazing = true;
         float pi = 3.14f;
 
-        System.out.println(currentWorldPopulation);
-        System.out.println(carProducedThisYear);
-        System.out.println(firstLetterInAlphabet);
-        System.out.println(javaAmazing);
-        System.out.println(pi);
+        System.out.println();
+        System.out.println("Task 5. Declaration of variables of primitive types");
+        System.out.println("long - " + currentWorldPopulation);
+        System.out.println("int - " + carProducedThisYear);
+        System.out.println("char - " + firstLetterInAlphabet);
+        System.out.println("boolean - " + javaAmazing);
+        System.out.println("float - " + pi);
+
+        // Task 6. Calculate the value of "total" using a given formula
+        double price = 25.15d;
+        double tax = 3.5d;
+        int quantity = 267;
+        double sum = 0d;
+
+        System.out.println();
+        System.out.println("Task 6. Calculate the value of \"total\" using a given formula \"total = price + tax * quantity + sum\", where:");
+        System.out.println("price = " + price);
+        System.out.println("tax = " + tax);
+        System.out.println("quantity = " + quantity);
+        System.out.println("sum = " + sum);
+
+        /* If we don`t calculate value of "sum" than we do an extra operation "+ sum" in "total" calculation,
+        * but if we`ll use some formula for "sum" calculation, for ex. "sum = quantity * price", then
+        * the variable "sum" will make sense in "total" calculation, consequently and "total" calculation
+        * could be changed for ex. like "total = sum + tax * quantity" */
+        double total = price + tax * quantity + sum;
+//        total = price + tax*quantity + sum;
+
+        System.out.println();
+        System.out.println("total = " + total);
     }
 }
