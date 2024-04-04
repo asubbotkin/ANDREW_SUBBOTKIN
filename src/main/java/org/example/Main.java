16183e7f1888e08b616510db729469ad4dfcae69
Andrew Subbotkin <asubbotkin@gmail.com>
2024-04-04 09:06:30 +0300

    homework5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("This is my 2-nd homework");
         System.out.println("This is my 3-rd homework");
         System.out.println("This is my 4-th homework");
+        System.out.println("This is my 5-th homework");
     }
 }
