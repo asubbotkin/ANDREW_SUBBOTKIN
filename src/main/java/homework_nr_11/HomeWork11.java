450bac47d07da74eedd75eb5245132d12f1cb00d
Andrew Subbotkin <asubbotkin@gmail.com>
2024-04-17 20:21:55 +0300

    Initial commit

---
+++ src/main/java/homework_nr_11/HomeWork11.java
@@ -0,0 +1,7 @@
+package homework_nr_11;
+
+public class HomeWork11 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/homework_nr_11/Person.java
@@ -0,0 +1,15 @@
+package homework_nr_11;
+
+public class Person {
+    String name;
+    String surname;
+    int age;
+    boolean isMale;
+
+    public Person(String name, String surname, int age, boolean isMale) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.isMale = isMale;
+    }
+}
