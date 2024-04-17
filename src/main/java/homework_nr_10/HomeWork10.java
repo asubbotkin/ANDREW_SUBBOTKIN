1463481058cf6cfd8cd035f99b8c155ce25e0055
Andrew Subbotkin <asubbotkin@gmail.com>
2024-04-17 10:30:30 +0300

    Initial commit

---
+++ src/main/java/homework_nr_10/HomeWork10.java
@@ -0,0 +1,13 @@
+package homework_nr_10;
+
+public class HomeWork10 {
+    public static void main(String[] args) {
+
+        int testable = 3;
+
+        Predicate checkOdd = (int x) -> {
+          return true;
+        };
+        System.out.println();
+    }
+}
---
+++ src/main/java/homework_nr_10/Predicate.java
@@ -0,0 +1,5 @@
+package homework_nr_10;
+@FunctionalInterface
+public interface Predicate {
+    boolean odd(int x);
+}
