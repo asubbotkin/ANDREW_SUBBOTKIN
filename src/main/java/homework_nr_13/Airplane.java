3cd9e53c22383ffef968c1280653bdb36e02f4b7
Andrew Subbotkin <asubbotkin@gmail.com>
2024-04-22 10:43:26 +0300

    Initial commit

---
+++ src/main/java/homework_nr_13/Airplane.java
@@ -0,0 +1,13 @@
+package homework_nr_13;
+
+public class Airplane {
+    protected String manufacturer;
+    protected String model;
+    protected int nrOfEngines;
+
+    public Airplane(String manufacturer, String model, int nrOfEngines) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.nrOfEngines = nrOfEngines;
+    }
+}
---
+++ src/main/java/homework_nr_13/HomeWork13.java
@@ -0,0 +1,7 @@
+package homework_nr_13;
+
+public class HomeWork13 {
+    public static void main(String[] args) {
+
+    }
+}
