bb61b9b7f750bb0f14699d5ffe3b57b2126817e3
Andrew Subbotkin <50634078+asubbotkin@users.noreply.github.com>
2024-05-19 10:58:22 +0300

    Homework 18 added TaskManager class

--- src/main/java/homework_nr_18/HomeWork18.java
+++ src/main/java/homework_nr_18/HomeWork18.java
@@ -1,7 +1,35 @@
 package homework_nr_18;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class HomeWork18 {
     public static void main(String[] args) {
+//        "Have a coffee", "Go to job", "Go to the gym", "Meet friends"
+        List<String> tasksList;
+        TaskManager taskManagerObject = new TaskManager(new LinkedList<>());
+
+        taskManagerObject.addTask("");
+        taskManagerObject.getAllTasks();
+
+        System.out.println("-----------------------");
+        taskManagerObject.removeTask(1);
+        taskManagerObject.getAllTasks();
+
+        System.out.println("-----------------------");
+        taskManagerObject.addTask("Go to job");
+        taskManagerObject.getAllTasks();
+
+        System.out.println("-----Cloned---------------");
+        LinkedList<String> clonedList = taskManagerObject.cloneTasksList();
+
+        for (String task : clonedList) {
+            System.out.println(task);
+        }
+
+        System.out.println("-----Tasks count---------------");
+        System.out.println(taskManagerObject.getTasksCount());
+
 
     }
 }
--- src/main/java/homework_nr_18/TaskManager.java
+++ src/main/java/homework_nr_18/TaskManager.java
@@ -1,4 +1,30 @@
 package homework_nr_18;
 
+import java.util.LinkedList;
+
 public class TaskManager {
+    private final LinkedList<String> tasksList;
+
+    public TaskManager(LinkedList<String> tasksList) {
+        this.tasksList = tasksList;
+    }
+
+    public synchronized void addTask(String task){
+        this.tasksList.add(task);
+    }
+
+    public synchronized void removeTask(int index){
+        this.tasksList.remove(index);
+    }
+
+    public synchronized int getTasksCount(){
+        return this.tasksList.size();
+    }
+    public void getAllTasks(){
+        for (String task : tasksList) System.out.println(task);
+    }
+
+    public synchronized LinkedList cloneTasksList(){
+        return (LinkedList) this.tasksList.clone();
+    }
 }
