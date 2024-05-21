e2e0f312792cff3c7189b122a052be85a2e97f84
Andrew Subbotkin <50634078+asubbotkin@users.noreply.github.com>
2024-05-21 10:13:57 +0300

    Homework 18 added Callable and FutureTask

--- src/main/java/homework_nr_18/HomeWork18.java
+++ src/main/java/homework_nr_18/HomeWork18.java
@@ -1,54 +1,57 @@
 package homework_nr_18;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class HomeWork18 {
     public static void main(String[] args) {
-//
-        List<String> tasksList = new ArrayList<>(Arrays.asList("Have a coffee", "To go to the job", "To  go to the gym", "Meet friends"));
-        TaskManager taskManagerObject = new TaskManager(new LinkedList<>());
 
+        List<String> tasksList = new ArrayList<>(Arrays.asList("task0", "task1", "task2", "task3", "task4"));
+        TaskManager taskManagerObject = new TaskManager(new LinkedList<>());
         LinkedList<String> clonedList;
 
-        Runnable addTask = () ->  {
-            for (String task : tasksList) {
-                taskManagerObject.addTask(task);
-            }
-        };
+        taskManagerObject.tasksList.addAll(tasksList);
 
-        Runnable removeTask = () -> {
-            taskManagerObject.removeTask(1);
-        };
+        String addNewTask = "task5";
+        String removeOldTask = "task1";
 
-        Runnable getTasksCount = taskManagerObject::getTasksCount;
+        Runnable addTask = () -> taskManagerObject.addTask(addNewTask);
+        Runnable removeTask = () -> taskManagerObject.removeTask(removeOldTask);
 
-        Runnable cloneTasksList = () -> {
-//            taskManagerObject.cloneTasksList();
-        };
+        Callable<Integer> tasksCount = taskManagerObject::getTasksCount;
+        Callable<LinkedList<String>> cloneTasksList = taskManagerObject::cloneTasksList;
+        FutureTask<Integer> getTasksCount = new FutureTask<>(tasksCount);
+        FutureTask<LinkedList<String>> getClonedTaskList = new FutureTask<>(cloneTasksList);
 
 
+        Thread addTaskThread = new Thread(addTask);
+        Thread removeTaskThread = new Thread(removeTask);
+        Thread getTasksCountThread = new Thread(getTasksCount);
+        Thread cloneTasksListThread = new Thread(getClonedTaskList);
 
-        taskManagerObject.getAllTasks();
+        removeTaskThread.start();
+        addTaskThread.start();
+        getTasksCountThread.start();
+        cloneTasksListThread.start();
 
-        System.out.println("-----------------------");
-        taskManagerObject.getAllTasks();
-
-        System.out.println("-----------------------");
-        taskManagerObject.addTask("Go to job");
-        taskManagerObject.getAllTasks();
-
-        System.out.println("-----Cloned---------------");
-
-        for (String task : clonedList) {
-            System.out.println(task);
+        try {
+            int count = getTasksCount.get();
+            System.out.println("------Tasks count-----------");
+            System.out.println(count);
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Can`t get tasks count!");
         }
 
-        System.out.println("-----Tasks count---------------");
-        System.out.println(taskManagerObject.getTasksCount());
-
-
+        try {
+            clonedList = getClonedTaskList.get();
+            System.out.println("--------Cloned tasks list-------");
+            for (String task : clonedList) {
+                System.out.println(task);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Can`t clone tasks list!");
+        }
     }
 }
--- src/main/java/homework_nr_18/TaskManager.java
+++ src/main/java/homework_nr_18/TaskManager.java
@@ -3,28 +3,61 @@ package homework_nr_18;
 import java.util.LinkedList;
 
 public class TaskManager {
-    private final LinkedList<String> tasksList;
+
+    protected LinkedList<String> tasksList;
 
     public TaskManager(LinkedList<String> tasksList) {
         this.tasksList = tasksList;
     }
 
-    public synchronized void addTask(String task){
-        this.tasksList.add(task);
+    public synchronized void addTask(String task) {
+        try {
+            System.out.println("Adding task: " + task);
+            Thread.sleep(100);
+            this.tasksList.add(task);
+        } catch (InterruptedException e) {
+            System.out.println("Error adding task!");
+        }
     }
 
-    public synchronized void removeTask(int index){
-        this.tasksList.remove(index);
+    public synchronized void removeTask(String task) {
+        try {
+            System.out.println("Removing task: " + task);
+            Thread.sleep(100);
+            if (!this.tasksList.isEmpty())
+                this.tasksList.remove(task);
+        } catch (InterruptedException e) {
+            System.out.println("Error removing task!");
+        }
     }
 
-    public synchronized int getTasksCount(){
+    public synchronized int getTasksCount() {
+        try {
+            System.out.println("Getting tasks count...");
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            System.out.println("Error getting tasks count!");
+        }
         return this.tasksList.size();
     }
-    public void getAllTasks(){
-        for (String task : tasksList) System.out.println(task);
+
+    public synchronized void getAllTasks() {
+        try {
+            System.out.println("Getting tasks list...");
+            Thread.sleep(30);
+            for (String task : tasksList) System.out.println(task);
+        } catch (InterruptedException e) {
+            System.out.println("Error getting tasks list!");
+        }
     }
 
-    public synchronized LinkedList cloneTasksList(){
-        return (LinkedList) this.tasksList.clone();
+    public synchronized LinkedList<String> cloneTasksList() {
+        try {
+            System.out.println("Clone tasks list...");
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return (LinkedList<String>) this.tasksList.clone();
     }
 }
